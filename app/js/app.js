199d633af719cec43abcb0be3cd83ac4e12a5fd6
Zdamian <damianzielin.js@gmail.com>
2017-01-09 13:34:14 +0100

    added form

--- app/js/app.js
+++ app/js/app.js
@@ -6,8 +6,10 @@ $(function() {
     var $btnPut = $('.app-put');
     var $btnDelete = $('.app-delete');
 
-    var $input = $('.app-input');
+    var $inputArtist = $('.app-artist-input');
+    var $inputAlbum = $('.app-album-input');
     var $list = $('.app-list');
+    var $trackDetails = $('.app-track-details');
 
     $btnGetAll.on('click', function() {
 
@@ -26,11 +28,24 @@ $(function() {
 
                 var songs = res;
                 songs.forEach(function(song) {
+                    var dateAt = song.created_at;
+                    var today = new Date(dateAt);
+                    var day = today.getDate();
+                    var month = today.getMonth() + 1;
+                    var year = today.getFullYear();
+                    var hour = today.getHours();
+                    var minute = today.getMinutes();
+
+                    if (hour < 10) hour = "0" + hour;
+
+                    if (minute < 10) minute = "0" + minute;
+
+                    var date = 'Music added: ' + day + "/" + month + "/" + year + " | " + hour + ":" + minute;
 
                     var $li = $('<li/>');
                     $li.attr('song-id', song._id);
                     $li.append('<span>' + song.artist + '</span>');
-                    $li.append(' <i>' + song.created_at + '</i>');
+                    $li.append(' <i>' + date + '</i>');
                     $li.append(' <button class="app-get btn btn-primary">GET</button>');
                     $li.append(' <button class="app-put btn btn-default">PUT</button>');
                     $li.append(' <button class="app-delete btn btn-warning">DELETE</button>');
@@ -63,7 +78,13 @@ $(function() {
 
                 var song = res;
 
-                $this.closest('li').find('span').text(song.artist);
+                var $details = $('<div/>');
+                    $details.attr('song-id', song._id);
+                    $details.append('<span>' + song.album + '</span>');
+                    $details.append(' <button class="app-get btn btn-primary">GET</button>');
+                    $details.append(' <button class="app-put btn btn-default">PUT</button>');
+                    $details.append(' <button class="app-delete btn btn-warning">DELETE</button>');
+                    $trackDetails.append($details);
             },
             error: function(err) {
                 console.log('error: ', err);
@@ -78,14 +99,16 @@ $(function() {
 
         $this.addClass('disabled');
 
-        var artist_text = $input.val();
+        var artist_text = $inputArtist.val();
+        var album_text = $inputAlbum.val();
 
         $.ajax({
             url: 'http://localhost:5555/songs',
             method: 'POST',
             dataType: 'JSON',
             data: {
-                'artist': artist_text
+                'artist': artist_text,
+                'album': album_text
             },
             success: function(res) {
                 console.log('success: ', res);
@@ -102,7 +125,8 @@ $(function() {
                 $li.append(' <button class="app-delete btn btn-warning">DELETE</button>');
                 $list.append($li);
 
-                $input.val('');
+                $inputArtist.val('');
+                $inputAlbum.val('');
             },
             error: function(err) {
                 console.log('error: ', err);
